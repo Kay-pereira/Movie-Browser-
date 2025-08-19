9d06e23ae79c93d76ecc6a4c88b47402737f0551
Kay-pereira <edmundpereira18@gmail.com>
2025-08-19 15:37:55 +0000

    still fixing ref error in brower console

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode, useRef, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './css/index.css'
-import App from './App.jsx'
-import { useRef, useEffect } from 'react';
+import './css/index.css';
+import App from './App.jsx';
 
+function RootWrapper() {
+  const setScriptRoute = useRef(false);
 
-const setScriptRoute = useRef(false);
+  useEffect(() => {
+    if (!setScriptRoute.current) {
+      // Only inject once
+      const script = injectSpeedInsights({
+        framework: "react",
+      });
+      setScriptRoute.current = true;
+    }
+  }, []);
 
-useEffect(() => {
-  if (!setScriptRoute.current) {
-    const script = injectSpeedInsights({
-      framework: props.framework ?? "react",
-    });
-    setScriptRoute.current = true;
-  }
-}, []);
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+  <StrictMode>
+    <RootWrapper />
+  </StrictMode>
+);
